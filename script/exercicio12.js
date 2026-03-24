64dc297927ebbf54f1747202924c62363aa3a046
Kaua <oclesianok@gmail.com>
2026-03-23 22:08:51 -0300

    done

--- script/exercicio12.js
+++ script/exercicio12.js
@@ -1,9 +1,9 @@
-// Peça um número e informe se ele é positivo ou negativo.
+ //Peça um número e informe se ele é positivo ou negativo.
 const numero = Number(prompt("Digite um numero"))
 if(numero >= 0) {
     alert ("positivo")
 }else if(numero <= 0) {
     alert("negativo")
 }else {
-    alert ("erro")
+    alert ("erro//")
 }
